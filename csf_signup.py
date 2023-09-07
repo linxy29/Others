d597a4023f701723f91e64ffcba4fcd12d2c3567
linxy29 <xinyi_lin@outlook.com>
2023-09-07 17:43:43 +0800

    add check with previous slot

--- csf_signup.py
+++ csf_signup.py
@@ -7,7 +7,19 @@ from email.mime.text import MIMEText
 import schedule
 import time
 
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+import schedule
+import time
+
+# This set will store the slots detected in the previous check
+previous_slots = set()
+
 def check_slots():
+    global previous_slots  # Declare the global variable to update it within the function
+
     # Get the webpage content
     html = urlopen("https://fcbooking.cse.hku.hk/")
     html_text = bytes.decode(html.read())
@@ -15,7 +27,7 @@ def check_slots():
     b_active_section = soup.find('div', {'id': 'c10002Content'})
     slot_sections = b_active_section.find_all('div', class_='py-2 text-center grey lighten-2 font-size-list')
     
-    available_slots_with_dates = []
+    available_slots_with_dates = set()
 
     for section in slot_sections:
         date = section.get_text(strip=True)
@@ -24,14 +36,15 @@ def check_slots():
             time_div = slot.find('div', class_='col text-center')
             availability_div = time_div.find_next_sibling('div')
             if "FULL" not in availability_div.get_text():
-                available_slots_with_dates.append(f"{date} - {time_div.get_text()}")
+                available_slots_with_dates.add(f"{date} - {time_div.get_text()}")
 
-    if available_slots_with_dates:
+    new_slots = available_slots_with_dates - previous_slots
+    if new_slots:  # If there are new slots since the last check
         # Prepare the email
         sender_email = "forsdemail@163.com"
         recipient_email = "xl2836@outlook.com"
         subject = "Available Slots in HKU B-Active"
-        body = "Available slots:\n" + '\n'.join(available_slots_with_dates)
+        body = "Available slots:\n" + '\n'.join(available_slots_with_dates)  # Use all available slots for the email body
 
         msg = MIMEText(body)
         msg['From'] = sender_email
@@ -42,6 +55,9 @@ def check_slots():
         with smtplib.SMTP_SSL('smtp.163.com', 465) as server:
             server.login(sender_email, "GEQRHYVMTTIGIYKU")
             server.sendmail(sender_email, recipient_email, msg.as_string())
+
+        # Update the previous slots with the currently detected slots for the next iteration
+        previous_slots = available_slots_with_dates
 
 # Schedule the function to run every 30 minutes from 6am to 11:59pm
 schedule.every(30).minutes.do(check_slots).tag('slot_checking')
